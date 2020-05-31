9f9a7e2dc8ced60aa0db97a1b941b43223d152f7
rmit-s3598660-Banu-Tennakoon <banutennakoon@gmail.com>
2020-05-31 13:02:00 +1000

    101 directives

---
+++ directives-start/src/app/basic-highlight/basic-highlight.directive.ts
@@ -0,0 +1,11 @@
+import { Directive, ElementRef, OnInit, HostListener } from "@angular/core";
+
+@Directive({
+  selector: "[appBasicHighlight]",
+})
+export class BasicHighlightDirective implements OnInit {
+  constructor(private elementRef: ElementRef) {}
+  ngOnInit() {
+    this.elementRef.nativeElement.style.backgroundColor = "green";
+  }
+}
