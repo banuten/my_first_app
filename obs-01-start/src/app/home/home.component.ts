d4278e2c900b87b719c5b80265afe188c77d3238
rmit-s3598660-Banu-Tennakoon <banutennakoon@gmail.com>
2020-06-10 21:25:46 +1000

    179 observable

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { interval, Subscription, Observable } from "rxjs";
+import { map, filter } from "rxjs/operators";
+import { Data } from "@angular/router";
 
 @Component({
   selector: "app-home",
@@ -15,17 +17,42 @@ export class HomeComponent implements OnInit, OnDestroy {
     // this.firstObsSubscription = interval(1000).subscribe((count) => {
     //   console.log(count);
     // });
+    // tslint:disable-next-line: deprecation
     const customIntervalObservable = Observable.create((observer) => {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count === 5) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error("Count is greater than 3"));
+        }
         count++;
       }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe((data) => {
-      console.log(data);
-    });
+    this.firstObsSubscription = customIntervalObservable
+      .pipe(
+        filter((data) => {
+          return data > 0;
+        }),
+        map((data: number) => {
+          return "Round " + (data + 1);
+        })
+      )
+      .subscribe(
+        (data: Data) => {
+          console.log(data);
+        },
+        (error) => {
+          console.log(error);
+          alert(error.message);
+        },
+        () => {
+          console.log("COmpleted!"); //Error cancels observable, and doesnt complete it
+        }
+      );
   }
 
   ngOnDestroy() {
